b4ca0e18cba63b2abd10996659334c029e333f27
naghamatef <naghamatef92@gmail.com>
2022-01-06 18:38:17 +0200

    1/6/2021 index

--- js/custom.js
+++ js/custom.js
@@ -1,23 +1,31 @@
 // menu
-$(".menu-icon").click(function () {
-    $("nav,.menu-icon").toggleClass("active");
+$(".nav-icon").click(function () {
+    $("nav,.nav-icon").toggleClass("active");
+});
 
+$(".close-nav").click(function () {
+    $("nav,.nav-icon").removeClass("active");
 });
 
 $(function () {
     var $win = $(window); // or $box parent container
-    var $box = $("nav,.menu-icon");
+    var $box = $(".inner-nav,.nav-icon");
     $win.on("click.Bst", function (event) {
         if (
             $box.has(event.target).length === 0 && //checks if descendants of $box was clicked
             !$box.is(event.target) //checks if the $box itself was clicked
         ) {
-            $("nav,.menu-icon").removeClass("active");
+            $("nav,.nav-icon").removeClass("active");
            
         }
     });
 });
 
+$("nav .menu-item-has-child").click(function () {
+    $(this).find(".main-submenu").slideToggle();
+    $(this).siblings().find(".main-submenu").slideUp();
+
+});
 //tooltip
 tippy('.tooltip-link', {
     arrow: true,
--- js/index.js
+++ js/index.js
@@ -20,7 +20,6 @@
      }, 1000);
  });
 
- $(document).ready(function () {
 
      $('.product-carousel').slick({
          dots: true,
@@ -30,12 +29,29 @@
          rtl: ($("html").attr("dir")) == "rtl" ? true : false,
          slidesToShow: 4,
          responsive: [{
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+            }
+        },
+        {
              breakpoint: 767,
              settings: {
-                 slidesToShow: 1,
-                 slidesToScroll: 1
+                 slidesToShow: 2,
              }
-         }]
+         },
+         {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+            }
+        }]
 
      });
 
@@ -47,79 +63,52 @@
          rtl: ($("html").attr("dir")) == "rtl" ? true : false,
          slidesToShow: 4,
          responsive: [{
+            breakpoint: 500,
+            settings: {
+                slidesToShow: 1,
+            }
+        },
+        {
              breakpoint: 767,
              settings: {
-                 slidesToShow: 1,
-                 slidesToScroll: 1
+                 slidesToShow: 2,
              }
-         }]
+         },
+         {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+            }
+        }]
      });
 
 
      $('.parteners-slider').slick({
-         dots: true,
+         dots: false,
          arrows: false,
          autoplay: true,
          rtl: ($("html").attr("dir")) == "rtl" ? true : false,
-         slidesToShow: 4,
-         slidesToScroll: 4,
+         slidesToShow: 5,
          responsive: [{
                  breakpoint: 992,
                  settings: {
-                     slidesToShow: 3,
-                     slidesToScroll: 3
+                     slidesToShow: 4,
                  }
              },
+             {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
              {
                  breakpoint: 576,
                  settings: {
                      slidesToShow: 2,
-                     slidesToScroll: 2
                  }
              }
          ]
 
      });
 
-     $('.works-sec').lightGallery({
-         selector: '.main-works-img',
-         thumbnail: false,
-         animateThumb: false,
-         showThumbByDefault: false,
-         hideControlOnEnd: true,
-         loop: false,
-         slideEndAnimatoin: false,
-         download: false,
-         flipVertical: false,
-         flipHorizontal: false,
-         rotate: false,
-         actualSize: false,
-         share: false,
-     });
- });
-
- // Wrap every letter in a span
- var textWrapper = document.querySelector('.title');
- textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span>$&</span>");
-
- anime.timeline({
-         loop: true
-     })
-     .add({
-         targets: '.title span',
-         translateX: [40, 0],
-         translateZ: 0,
-         opacity: [0, 1],
-         easing: "easeOutExpo",
-         duration: 2000,
-         color: '#FF0000',
-         delay: (el, i) => 1000 + 50 * i
-     }).add({
-         targets: '.title span',
-         translateX: [0, -30],
-         opacity: [1, 0],
-         easing: "easeInExpo",
-         duration: 2100,
-         color: '#141414',
-         delay: (el, i) => 500 + 50 * i
-     });
+   
